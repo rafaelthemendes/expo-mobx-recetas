7eeea902aaa074527c01c014f9d604f6054c8a92
Rafael Mendes <rafael.mendes@ifactory.com.br>
2019-04-11 12:49:27 -0300

    Added themes: styles, fonts, metrics

---
+++ src/themes/Colors.js
@@ -0,0 +1,14 @@
+export default {
+  transparent: 'rgba(0,0,0,0)',
+  whiteFull: '#ffffff',
+  blackFull: '#333333',
+  redFull: '#ff0000',
+  lightOrange: '#ff8c2b',
+  darkOrange: '#ff6322',
+  black: '#333333',
+  darkGrey: '#9e9e9e',
+  mediumGrey: '#bdbdbd',
+  gray: '#e0e0e0',
+  lightGrey: '#f4f6f6',
+  darkWhite: '#f7f7f7'
+};
---
+++ src/themes/Metrics.js
@@ -0,0 +1,7 @@
+export default {
+  statusBarHeight: 20,
+  navBarHeight: 56,
+  tabBarHeight: 56,
+  baseSpace: 16,
+  smallSpace: 8
+};
